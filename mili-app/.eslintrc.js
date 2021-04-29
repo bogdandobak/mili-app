b42c0f37c21acda22430ee1548b93c838e0190cd
Bohdan Dobak <bdobak@softonix.org>
2021-04-29 20:10:59 +0300

    setup project

---
+++ mili-app/.eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': ['error', { code: 120, 'ignoreStrings': true }],
+    'vue/max-attributes-per-line': 'off',
+    'vue/attribute-hyphenation': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/no-v-html': 'off',
+    'vue/custom-event-name-casing': 'off',
+    'vue/v-slot-style': ['error', {
+      'atComponent': 'shorthand',
+      'default': 'shorthand',
+      'named': 'shorthand'
+    }],
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/attribute-hyphenation': 'off',
+    'vue/custom-event-name-casing': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/interface-name-prefix': [
+      'error', { 'prefixWithI': 'always' }
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        prefix: ['I'],
+      },
+      {
+        selector: 'typeAlias',
+        format: ['PascalCase'],
+        prefix: ['T'],
+      },
+      {
+        selector: 'enum',
+        format: ['PascalCase'],
+        prefix: ['E'],
+      },
+    ]
+  }
+}
---
+++ mili-app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <HelloWorld />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default defineComponent({
+  components: {
+    HelloWorld
+  }
+})
+</script>
